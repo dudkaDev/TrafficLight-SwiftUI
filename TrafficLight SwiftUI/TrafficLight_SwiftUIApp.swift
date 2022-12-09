42a8a6f5fb82ffdedb70a234665c0f84d279be2a
Andrey Abakumov <andrey9864rus@mail.ru>
2022-12-09 19:04:21 +0400

    Initial Commit

---
+++ TrafficLight SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight SwiftUI
+//
+//  Created by Андрей Абакумов on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
